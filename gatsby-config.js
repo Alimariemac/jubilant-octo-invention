b60c4b8be1e98bf353f1a0d694dd959bada88501
Alicia MacCara <Aliciamaccara@gmail.com>
2019-12-02 19:11:30 -0500

    rich text update

---
+++ gatsby-config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  siteMetadata: {
+    title: `Alicia MacCara's portfolio`,
+    description: `Designer, programmer, maker.`,
+    author: `@alimariemac`,
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options:{
+        spaceId:'5nnt7bf94db7',
+        accessToken:'_uSxNLU_SSs5WizY9oc4v5JC0dpl8pDQ0Qy2TlhEhQo',
+        downloadLocal:'true'
+      }
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-mdx`,
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `alicia-website`,
+        short_name: `alicia`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        display: `minimal-ui`,
+        icon: `src/images/blob.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
